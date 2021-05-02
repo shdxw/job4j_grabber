649f710b5d0490f7af2c37e075cabb4d9eaa83c5
shdxw <56305521+shdxw@users.noreply.github.com>
2021-05-02 15:02:57 +1000

    2.3. Загрузка деталей поста. [#381537]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -13,11 +13,17 @@ public class SqlRuParse {
             Elements row = table.select("tr");
             row.remove(0);
             for (Element element : row) {
-                String href = String.format("url:%s text:%s date:%s",
+                String href = String.format("url:%s name:%s lastdate:%s",
                         element.child(1).child(0).attr("href"),
                         element.child(1).child(0).text(),
                         element.child(5).text());
-                System.out.println(href);
+                System.out.println(href); //работаем с таблицей постов
+                Document localHref = Jsoup.connect(element.child(1).child(0).attr("href")).get();
+                Element elementsOfPost = localHref.getElementsByClass("msgTable").first()
+                        .child(0);
+                System.out.println(String.format("text: %s date: %s", elementsOfPost.child(1).child(1).text(),
+                        elementsOfPost.child(2).child(0).text().split("\\[")[0].trim()));//работаем с постом
+
             }
         }
     }
